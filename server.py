913d7468ca93b73bb95eb7c241199c6609d3ae89
Eugenio Pastoral <gino@achrovisual.com>
2022-02-17 12:32:46 +0800

    Revised synchronization flow

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, sys, os, json, time
+import socket, sys, os, json, time, hashlib
 from _thread import *
 
 class Server():
@@ -18,6 +18,9 @@ class Server():
 
         # Initialize list for files that need to be uploaded across all servers
         self.upload_queue = []
+        self.files_list = []
+        # self.get_files_list()
+        # print(self.files_list)
 
         # Create server socket and bind address and port
         server_socket = socket.socket()
@@ -41,32 +44,37 @@ class Server():
         # Wait for 5 seconds before connecting to peers
         time.sleep(5)
 
-        # Connect to peers if not yet connected
-        if self.server_sockets == []:
-            try:
-                # Iterate through server peer ports
-                for port in self.SERVER_PEERS:
-                    # Connect to peer
-                    server_socket = socket.socket()
-                    print(f"[+] Connecting to 0.0.0.0:{port}.")
-                    server_socket.connect(("0.0.0.0", port))
-                    print(f"[+] Connected to 0.0.0.0:{port}.")
+        while True:
+            # Connect to peers if not yet connected
+            if self.server_sockets == []:
+                try:
+                    # Iterate through server peer ports
+                    for port in self.SERVER_PEERS:
+                        # Connect to peer
+                        server_socket = socket.socket()
+                        print(f"[+] Connecting to 0.0.0.0:{port}.")
+                        server_socket.connect(("0.0.0.0", port))
+                        print(f"[+] Connected to 0.0.0.0:{port}.")
 
-                    # Append server peer socket to list
-                    self.server_sockets.append(server_socket)
+                        # Append server peer socket to list
+                        self.server_sockets.append(server_socket)
 
-                # Keep synchronizing servers
+                    # Keep synchronizing servers
+                    # time.sleep(10)
+                    self.synchronize_servers()
+
+                except Exception as e:
+                    print(e)
+                    for port in self.SERVER_PEERS:
+                        port.close()
+            else:
                 self.synchronize_servers()
 
-            except Exception as e:
-                print(e)
-                for port in self.SERVER_PEERS:
-                    self.port.close()
 
     def download(self, client_socket, address, filename):
         print(f"[*] Sending {filename} to {address}.")
         try:
-            filesize = os.path.getsize(filename)
+            filesize = os.path.getsize(self.SERVER_NAME + "/" + filename)
 
             with open(self.SERVER_NAME + "/" + filename, "rb") as f:
                 while True:
@@ -80,40 +88,51 @@ class Server():
     def upload(self, client_socket, address, filename):
         print(f"[*] Receiving {filename} from {address}.")
         try:
+            if client_socket not in self.server_sockets:
+                self.upload_queue.append(filename)
+
             with open(self.SERVER_NAME + "/" + filename, "wb") as f:
                 while True:
                     bytes_read = client_socket.recv(self.BUFFER_SIZE)
                     if not bytes_read:
                         break
                     f.write(bytes_read)
-            self.upload_queue.append(filename)
+
         except Exception as e:
             print(e)
             print("[!] Can't write file.")
 
     def synchronize_servers(self):
-        while True:
-            # print(self.upload_queue)
-            try:
-                if self.upload_queue:
-                    for filename in self.upload_queue:
-                        print("[*] New file was uploaded. Synchronizing servers.")
-                        for peer in self.server_sockets:
-                            filesize = os.path.getsize(filename)
-                            msg = {"command" : "upload", "filename" : filename, "filesize" : filesize}
+        # print(self.upload_queue)
+        try:
+            if self.upload_queue:
+                for filename in self.upload_queue:
+                    print("[*] New file was uploaded. Synchronizing servers.")
+                    for peer in self.server_sockets:
+                        filesize = os.path.getsize(self.SERVER_NAME + "/" + filename)
+                        msg = {"command" : "upload", "filename" : filename, "filesize" : filesize}
 
-                            peer.sendall(bytes(json.dumps(msg), encoding = "utf-8"))
+                        peer.sendall(bytes(json.dumps(msg), encoding = "utf-8"))
 
-                            self.download(peer, "127.0.0.1", filename)
-                        self.upload_queue.remove(filename)
-            except Exception as e:
-                print(e)
-                pass
+                        self.download(peer, "127.0.0.1", filename)
+                        peer.close()
+                    self.upload_queue.remove(filename)
+                    print("[*] Re-establishing connection with slaves...")
+                    self.server_sockets = []
+        except Exception as e:
+            print(e)
+            pass
 
+    def get_files_list(self):
+        for root, dir, files in os.walk(self.SERVER_NAME + "/"):
+            for file in files:
+                data = open(self.SERVER_NAME + "/" + file, "rb").read()
+                self.files_list.append({"filename" : file, "checksum" : hashlib.md5(data).hexdigest()})
+                self.upload_queue.append(file)
 
     def on_new_client(self, client_socket, address):
         while True:
-            message = client_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+            message = client_socket.recv(self.BUFFER_SIZE).decode("utf-8")
             if message:
                 parsed = json.loads(message)
                 print(str(parsed))
